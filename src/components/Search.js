c0a01851457aa20c2eebc6cfef1ebb0724c3ec2c
Joshua Grafton <grafton.josh@gmail.com>
2021-10-24 17:48:00 -0600

    added second useEffect to debounce term searched

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,36 +3,37 @@ import axios from 'axios'
 
 const Search = () => {
     const [term, setTerm] = useState('Zillow')
+    const [debouncedTerm, setDebouncedTerm] = useState(term)
     const [results, setResults] = useState([])
 
+    // This useEffect exists to prevent dependency array error
+    // if only tracking "term" in single useEffect
+    // Also prevents duplicate API requests for the same search term
+    // (i.e. 'Book' to 'Boo' back to 'Book' before setTimeout expires would not trigger new API call)
     useEffect(() => {
-            const search = async () => {
-                const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
-                    params: {
-                        action: 'query',
-                        list: 'search',
-                        origin: '*',
-                        format: 'json',
-                        srsearch: term
-                    }
-                })
-                setResults(data.query.search)
-            }
-            if (term && !results.length) {
-                search()
-            } else {
-                // Throttles API requests
-                const timeoutId = setTimeout(() => {
-                    if(term) {
-                        search()
-                    }
-                }, 500)
-                // Cleanup function below
-                return (() => {
-                    clearTimeout(timeoutId)
-                })
-            }
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term)
+        }, 1000)
+        return () => {
+            clearTimeout(timerId)
+        }
     }, [term])
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: debouncedTerm
+                }
+            })
+            setResults(data.query.search)
+        }
+        search()
+    }, [debouncedTerm])
 
     const renderedResults = results.map(result => {
         return (
