4f8b18fad415bd2d375b03d7dc5c2cf42cfb28bf
Joshua Grafton <grafton.josh@gmail.com>
2021-10-26 20:03:26 -0600

    added useRef to dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 const Dropdown = ({ options, label, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false)
+    const ref = useRef();
+
+    useEffect(() => {
+        document.body.addEventListener('click', (event) => {
+            console.log('BODY event', event);
+            if (ref.current.contains(event.target)) {
+                console.log('Contains ref, returning early');
+                return
+            }
+            setOpen(false)
+        },
+        { capture: true }
+        )
+    }, []);
     const renderOptions = options.map(option => {
         if(option.value === selected.value) {
             return null;
@@ -10,19 +24,25 @@ const Dropdown = ({ options, label, selected, onSelectedChange }) => {
             <div
                 key={option.value}
                 className="item"
-                onClick={() => onSelectedChange(option)}
+                onClick={() => {
+                    onSelectedChange(option)
+                }}
             >
                 {option.label}
             </div>
         )
     })
+    
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">{label}</label>
                 <div
                 className={`ui selection dropdown ${open ? 'visible active' : ''}`}
-                onClick={() => setOpen(!open)}
+                onClick={(e) => {
+                    console.log('Clicked Item. OPEN: ', open);
+                    setOpen(!open)
+                }}
                 >
                     <i className="dropdown icon" />
                     <div className="text">{selected.label}</div>
