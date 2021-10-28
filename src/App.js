b883ce84d497c40d27b5f4dbd72913d7c3bc70c5
Joshua Grafton <grafton.josh@gmail.com>
2021-10-27 19:43:13 -0600

    added router component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Accordion from './components/Accordion';
 import Translate from './components/Translate'
 import Dropdown from './components/Dropdown';
 import Search from './components/Search';
+import Route from './components/Route'
+import Header from './components/Header';
+import Link from './components/Link'
 
 const items = [
     {
@@ -19,11 +22,43 @@ const items = [
     }
 ]
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Blue',
+        value: 'blue'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+]
+
 const App = () => {
-    
+    const [selected, setSelected] = useState(options[0])
     return (
         <div>
-            <Translate />
+            <Header />
+           <Route path ="/">
+               <Accordion items={items} />
+           </Route>
+           <Route path="/list">
+               <Search /> 
+           </Route>
+           <Route path="/dropdown">
+               <Dropdown 
+                    label='Select a Color'
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+               /> 
+           </Route>
+           <Route path="/translate">
+               <Translate /> 
+           </Route>
         </div>
     )
 }
